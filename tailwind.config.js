363e487ee806c3ee74a62805871058e82607c1bc
yashsarvan58 <yashsarvan58@gmail.com>
2024-09-13 18:39:28 +0530

    updatetrasaction

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,ts,jsx,tsx}",
-//   ],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
 
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
 
