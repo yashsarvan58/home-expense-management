23764fd798280658a34717634632ca152af28e38
yashsarvan58 <yashsarvan58@gmail.com>
2024-09-17 13:28:36 +0530

    update

--- src/Transactions.jsx
+++ src/Transactions.jsx
@@ -1,40 +1,12 @@
 import React from 'react'
-import { Navigate } from 'react-router-dom'
-
 
 const Transactions = () => {
-  const [amount, setAmount] = useState("");
-  const [type, setType] = useState("expense");
-  const [remark, setRemark] = useState("");
-  const [date, setDate] = useState("");
-  const [transactions, setTransactions] = useState([]);
   return (
-    <>
-    
-    
-    <table className='transaction-table' width={"100%"} border={"1px"} >
-              <thead>
-                <tr>
-                  <th>Date</th>
-                  <th>Type</th>
-                  <th>Amount</th>
-                  <th>Remark</th>
-                </tr>
-              </thead>
-              <tbody>
-                {transactions.map((transaction, index) => (
-                  <tr key={index}>
-                    <td>{transaction.amount}</td>
-                    <td>{transaction.type}</td>
-                    <td>{transaction.remark}</td>
-                    <td>{transaction.date}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-    </>
+    <div>
+      
+    </div>
   )
-  
 }
 
 export default Transactions
+
